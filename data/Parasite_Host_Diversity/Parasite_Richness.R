573c12b6c13d33824e6d036ee0f4735c0bb6bf25
Ria Raut <133253292+riaraut7@users.noreply.github.com>
2026-08-20 10:45:54 -0700

    just downloaded and pulled Cameron's changes + edited some of the names, etc

--- data/Parasite_Host_Diversity/Parasite_Richness.R
+++ data/Parasite_Host_Diversity/Parasite_Richness.R
@@ -1,34 +1,89 @@
 getwd()
 
-library(readr) 
 library(tidyverse)
-library(stringr)
 
-"remove useless columns"
+# Remove unused columns
 DRE_Cleaned_1 <- DRE_Diversity_DF_25_March_2020 %>% 
-  select(-c("S2", "S3", "S4", "S", "mpd.obs.z", "dummy_1", "dummy_2", "dummy_3", "dummy_4", "dummy_5", "resources", "diversity", "month"))
+  select(-any_of(c(
+    "S2", "S3", "S4", "S", "mpd.obs.z",
+    "dummy_1", "dummy_2", "dummy_3", "dummy_4", "dummy_5",
+    "resources", "diversity", "month"
+  )))
 
-
-"remove all 2013 data"
+# Remove 2013 data
 DRE_Filtered <- DRE_Cleaned_1 %>%
-    filter(Year != 2013)
+  filter(Year != 2013)
 
-"now that we removed 2013 get rid of year all together and another useless column"
-DRE_Filtered <- DRE_Filtered %>% select(-c("Year", "S5m"))
-
-"replace the plot and plot id columns w uniqueid"
+# Remove Year, Plot, PlotID, and S5m, then rename uniqueid to Plot
 DRE_PreFinal <- DRE_Filtered %>%
-    select(-c("Plot", "PlotID", "S5m")) %>%
-    rename(Plot = uniqueid)
+  select(-any_of(c("Year", "Plot", "PlotID", "S5m"))) %>%
+  rename(Plot = uniqueid)
 
-"need to create columns for each species with entries of 0/1 and convert composition column data"
+# Create 0/1 species columns
 DRE_PreFinal <- DRE_PreFinal %>%
-    mutate(
-      ANVI = if_else(Div == 0, as.integer(spp == "ANVI"), as.integer(spp != "ANVI")),
-      SEPA = if_else(Div == 0, as.integer(spp == "SEPA"), as.integer(spp != "SEPA")),
-      TRFL = if_else(Div == 0, as.integer(spp == "TRFL"), as.integer(spp != "TRFL")),
-      PAAN = if_else(Div == 0, as.integer(spp == "PAAN"), as.integer(spp != "PAAN")),
-      SCIN = if_else(Div == 0, as.integer(spp == "SCIN"), as.integer(spp != "SCIN")),
-      SOPI = if_else(Div == 0, as.integer(spp == "SOPI"), as.integer(spp != "SOPI"))
+  mutate(
+    ANVI = if_else(Div == 0, as.integer(spp == "ANVI"), as.integer(spp != "ANVI")),
+    SEPA = if_else(Div == 0, as.integer(spp == "SEPA"), as.integer(spp != "SEPA")),
+    TRFL = if_else(Div == 0, as.integer(spp == "TRFL"), as.integer(spp != "TRFL")),
+    PAAN = if_else(Div == 0, as.integer(spp == "PAAN"), as.integer(spp != "PAAN")),
+    SCIN = if_else(Div == 0, as.integer(spp == "SCIN"), as.integer(spp != "SCIN")),
+    SOPI = if_else(Div == 0, as.integer(spp == "SOPI"), as.integer(spp != "SOPI"))
+  )
+
+# Creating Action and Outcome Columns
+DRE_PreFinal_2 <- DRE_PreFinal %>%
+  rename(
+    ANVI.action = ANVI,
+    SEPA.action = SEPA,
+    TRFL.action = TRFL,
+    PAAN.action = PAAN,
+    SCIN.action = SCIN,
+    SOPI.action = SOPI
+  ) %>%
+  mutate(
+    ANVI.outcome = ANVI.action,
+    SEPA.outcome = SEPA.action,
+    TRFL.outcome = TRFL.action,
+    PAAN.outcome = PAAN.action,
+    SCIN.outcome = SCIN.action,
+    SOPI.outcome = SOPI.action
+  )
+
+# Grouping Desirability Indexes, Then Get Rid of Them For Time 1
+desirability <- c(
+  'PlantS', 'S5', 'percov', 'Div', 'Res', 'divXres', 'n.plants'
+)
+
+DRE_PreFinal_3 <- DRE_PreFinal_2 %>%
+  relocate(
+    all_of(desirability), 
+    .after = SOPI.outcome
+  ) %>%
+  mutate(
+    across(all_of(desirability),
+      ~ if_else(time == 1, NA_real_, .) 
     )
+  )
+# Final draft sent for review
+  write_csv(DRE_PreFinal_3, "DRE_Final.csv")
+  
+# Deleting all 2012 rows to reduce redundancy, then removing the time column
+  DRE_PreFinal_4 <- DRE_PreFinal_3 %>%
+    filter(time != 1) %>%
+    select(-any_of(c('time')))
+  
+# Getting rid of the 2014 part of plot ID and moving it to the first column spot
+  DRE_Final_CSV <- DRE_PreFinal_4 %>%
+    relocate(
+      Plot,
+      .before = Blk
+    ) %>%
+    mutate(
+      Plot = str_remove_all(Plot, " 2014")
+    )
+
+# Writing Final CSV
+  write_csv(DRE_Final_CSV, 'DRE_Final_CSV.csv')
+
+    
 
--- data/Parasite_Host_Diversity/README.md
+++ data/Parasite_Host_Diversity/README.md
@@ -1,1 +1,35 @@
-Simple data cleaning project
+METADATA:
+
+Species			                  Code
+
+Andropogon virginicus	        ANVI
+
+Setaria parviflora		        SEPA
+
+Tridens flavus		            TRFL
+
+Packera anonyma		            PAAN
+
+Scutellaria integrifolia	    SCIN
+
+Solidago pinetorum		        SOPI
+
+Initial = 2012, Final = 2014
+
+Column Key
+
+spp = Species of focus
+
+Composition: Species of focus and Monoculture or Polyculture (M/P)
+
+PlantS: Total number of unique species in the plot
+
+S5: Parasite richness standardized to 5 random plants sampled within the plot (Desirability Variable)
+
+percov: percent cover within the plot (Desirability Varaibility)
+
+Div: Diversity, 1 = Polyculture, 0 = Monoculture
+
+Res: Resource (Phosphorous, Nitrogen, Potassium) Treatment, 1 = PNK treatment applied, 0 = PNK treatment not applied (Environmental Axis)
+
+Plot column represtents each unique plot through time: the number beginning it is the block number, the four letter code following that number is the species focused on in this plot, followed by its diversity classification (M or P), and finally the resource treatment (R = Treatment Applied, or O = Treatment not Applied)
